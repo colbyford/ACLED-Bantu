56c11d03f804f00e67d1d4e219edae1784cdff9f
Colby Ford <colbytylerford@gmail.com>
2018-02-24 18:11:18 -0500

    Update DimensionalityReductionScript.R

--- DimensionalityReductionScript.R
+++ DimensionalityReductionScript.R
@@ -1,27 +1,65 @@
 library(tidyr)
+library(readr)
 library(dplyr)
+library(purrr)
 library(psych)
 
-mtDNA <- c("AGCGTCGTCGATCGAGAGACCATGCTATGA","GCGTCGATCGATCGAGAGACCATGCTATGA", "GCGTCGTACGATCGAGAGACCATGCTATGA")
-mtDNA_cols <- paste0("mtDNA.pos", seq(1:nchar(mtDNA[1])))
+## Input as Table
+CombinedData <- read_csv("DimensionalityReduction_CombinedData_InnerJoin.csv")
+colnames(CombinedData)[1] <- "TaxaID" #Fix <U+FEFF> Issues
+mtDNA_cols <- paste0("mtDNA.pos", seq(1:nchar(CombinedData$mtDNA[1])))
+Ychr_cols <- paste0("Ychr.pos", seq(1:nchar(CombinedData$Ychr_STR[1])))
+Cultural_cols <- paste0("Cultural.pos", seq(1:nchar(CombinedData$Cultural_EthnogAtlas[1])))
 
-Ychr <- c("CHIAGGLEDPYW","GLEDPYWQLIAG","GLEPAYWQLIAG")
-Ychr_cols <- paste0("Ychr.pos", seq(1:nchar(Ychr[1])))
-
-Cultural <- c("0A129BCCCD90234","1B139BCCD90234A","21B139BCD90234A")
-Cultural_cols <- paste0("Cultural.pos", seq(1:nchar(Cultural[1])))
-
-CombinedData <- data.frame(mtDNA, Ychr, Cultural)
-
+## Separate Strings into Individual Columns
 SepCombinedData <- CombinedData %>% 
-  separate(., mtDNA, mtDNA_cols, sep = seq(1:nchar(mtDNA[1])), remove = TRUE) %>% 
-  separate(., Ychr, Ychr_cols, sep = seq(1:nchar(Ychr[1])), remove = TRUE) %>% 
-  separate(., Cultural, Cultural_cols, sep = seq(1:nchar(Cultural[1])), remove = TRUE)
+  separate(., mtDNA, mtDNA_cols, sep = seq(1:nchar(CombinedData$mtDNA[1])), remove = TRUE) %>% 
+  separate(., Ychr_STR, Ychr_cols, sep = seq(1:nchar(CombinedData$Ychr_STR[1])), remove = TRUE) %>% 
+  separate(., Cultural_EthnogAtlas, Cultural_cols, sep = seq(1:nchar(CombinedData$Cultural_EthnogAtlas[1])), remove = TRUE)
 
+## Remove NA Columns
+SepCombinedData <- SepCombinedData[!is.na(names(SepCombinedData))]
+
+## Clean Up Memory to Avoid Error
+rm("CombinedData", "Cultural_cols", "mtDNA_cols", "Ychr_cols")
+gc()
+memory.limit(size=99999)
+
+## Dummy Code Each Column and Write to Disk
+DummyCodedCombinedData <- as.data.frame(lapply(SepCombinedData, dummy.code)) #This takes forever
+DummyCodedCombinedData_orig <- DummyCodedCombinedData #Save for testing purposes
+DummyStartLoc <- which(colnames(DummyCodedCombinedData)=="Cultural.pos1.1")
+
+DummyCodedCombinedData <- cbind(SepCombinedData[,1:5],
+                                DummyCodedCombinedData[,DummyStartLoc:ncol(DummyCodedCombinedData)]) #Append Data
+DummyCodedCombinedData <- DummyCodedCombinedData %>% 
+  select(-starts_with("X")) #Remove Variable With Only 1 Value (Starts with X)
+write_csv(DummyCodedCombinedData, "DimensionalityReduction_CombinedData_InnerJoin_DummyCoded.csv", append = FALSE)
+#DummyCodedCombinedData <- read_csv("DimensionalityReduction_CombinedData_InnerJoin_DummyCoded.csv")
+
+## Clean Up Memory
+rm(SepCombinedData)
+gc()
+
+## Collapse Data By Guthrie Zones
+CollapsedDummyCodedCombinedData <- DummyCodedCombinedData %>% 
+  select(-TaxaID, -CulturalTaxa, -mtDNATaxa, -YchrTaxa) %>% 
+  group_by(GuthrieZone) %>% 
+  summarise_all(mean)
+
+write_csv(CollapsedDummyCodedCombinedData, "DimensionalityReduction_CombinedData_InnerJoin_DummyCoded_Collapsed.csv", append = FALSE)
+
+## Clean Up Memory
+rm(DummyCodedCombinedData)
+gc()
+
+
+=======
 DummyCodedCombinedData <- as.data.frame(lapply(SepCombinedData, dummy.code))
+>>>>>>> parent of 05c0ee7... Update DR Script and Add Combined Data
 
-#### MDS
-MDS_Combined_2 <- cmdscale(dist(DummyCodedCombinedData), eig=TRUE, k=2) # Dimensions
+## Multidimensional Scaling
+MDS_Combined_2 <- cmdscale(dist(DummyCodedCombinedData[1:100,1:25]), eig=TRUE, k=2) # Dimensions
 MDS_Combined_2 # view results
 
 
