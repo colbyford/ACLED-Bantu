e405ab4713e6bab65e128ccadf95262d6d7dfb16
Colby Ford <colbytylerford@gmail.com>
2018-02-25 12:18:09 -0500

    Moved Datasets to New Folder

--- DimensionalityReductionScript.R
+++ DimensionalityReductionScript.R
@@ -1,11 +1,18 @@
+###################################
+# Dimensionality Reduction Script #
+#        Bantu Migration          #
+#        By: Colby Ford           #
+###################################
 library(tidyr)
 library(readr)
 library(dplyr)
 library(purrr)
 library(psych)
+library(MASS)
+library(ggplot2)
 
 ## Input as Table
-CombinedData <- read_csv("DimensionalityReduction_CombinedData_InnerJoin.csv")
+CombinedData <- read_csv("datasets/DimensionalityReduction/DimensionalityReduction_CombinedData_InnerJoin.csv")
 colnames(CombinedData)[1] <- "TaxaID" #Fix <U+FEFF> Issues
 mtDNA_cols <- paste0("mtDNA.pos", seq(1:nchar(CombinedData$mtDNA[1])))
 Ychr_cols <- paste0("Ychr.pos", seq(1:nchar(CombinedData$Ychr_STR[1])))
@@ -34,20 +41,23 @@ DummyCodedCombinedData <- cbind(SepCombinedData[,1:5],
                                 DummyCodedCombinedData[,DummyStartLoc:ncol(DummyCodedCombinedData)]) #Append Data
 DummyCodedCombinedData <- DummyCodedCombinedData %>% 
   select(-starts_with("X")) #Remove Variable With Only 1 Value (Starts with X)
-write_csv(DummyCodedCombinedData, "DimensionalityReduction_CombinedData_InnerJoin_DummyCoded.csv", append = FALSE)
+write_csv(DummyCodedCombinedData, "datasets/DimensionalityReduction/DimensionalityReduction_CombinedData_InnerJoin_DummyCoded.csv", append = FALSE)
 #DummyCodedCombinedData <- read_csv("DimensionalityReduction_CombinedData_InnerJoin_DummyCoded.csv")
 
 ## Clean Up Memory
 rm(SepCombinedData)
 gc()
 
-## Collapse Data By Guthrie Zones
+## Collapse Data By TaxaID
 CollapsedDummyCodedCombinedData <- DummyCodedCombinedData %>% 
-  select(-TaxaID, -CulturalTaxa, -mtDNATaxa, -YchrTaxa) %>% 
-  group_by(GuthrieZone) %>% 
+  select(-GuthrieZone, -CulturalTaxa, -mtDNATaxa, -YchrTaxa) %>% 
+  group_by(TaxaID) %>% 
   summarise_all(mean)
 
-write_csv(CollapsedDummyCodedCombinedData, "DimensionalityReduction_CombinedData_InnerJoin_DummyCoded_Collapsed.csv", append = FALSE)
+write_csv(CollapsedDummyCodedCombinedData, "datasets/DimensionalityReduction/DimensionalityReduction_CombinedData_InnerJoin_DummyCoded_TaxaID_Collapsed.csv", append = FALSE)
+#CollapsedDummyCodedCombinedData <- as.data.frame(read_csv("DimensionalityReduction_CombinedData_InnerJoin_DummyCoded_TaxaID_Collapsed.csv"))
+rownames(CollapsedDummyCodedCombinedData) <- CollapsedDummyCodedCombinedData$TaxaID
+CollapsedDummyCodedCombinedData$TaxaID <- NULL
 
 ## Clean Up Memory
 rm(DummyCodedCombinedData)
@@ -55,15 +65,25 @@ gc()
 
 
 ## Multidimensional Scaling
-MDS_Combined_2 <- cmdscale(dist(DummyCodedCombinedData[1:100,1:25]), eig=TRUE, k=2) # Dimensions
-MDS_Combined_2 # view results
-
-
+## Classical MDS
+MDS_Classical_Combined_2 <- cmdscale(dist(CollapsedDummyCodedCombinedData), eig=TRUE, k=2) #2 Dimensions
+MDS_Classical_Combined_2 # view results
 
 # plot solution 
-x <- MDS_Combined_2$points[,1]
-y <- MDS_Combined_2$points[,2]
+x <- MDS_Classical_Combined_2$points[,1]
+y <- MDS_Classical_Combined_2$points[,2]
 plot(x, y, xlab="Coordinate 1", ylab="Coordinate 2", 
      main="Metric	MDS",	type="n")
-text(x, y, labels = row.names(DummyCodedCombinedData), cex=.7)
+text(x, y, labels = row.names(CollapsedDummyCodedCombinedData), cex=.7)
 
+## Non-Metric MDS
+MDS_NonMetric_Combined_2 <- isoMDS(dist(CollapsedDummyCodedCombinedData),
+                                   y = cmdscale(dist(CollapsedDummyCodedCombinedData),
+                                                k = 2),
+                                   k = 2,
+                                   maxit = 500,
+                                   trace = TRUE,
+                                   tol = 1e-3,
+                                   p = 2)
+MDS_NonMetric_Combined_2_Shep <- Shepard(dist(CollapsedDummyCodedCombinedData), MDS_NonMetric_Combined_2$points)
+plot(MDS_NonMetric_Combined_2_Shep)
