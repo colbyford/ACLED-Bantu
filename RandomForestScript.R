424486c3d774761807d47067e94e3012a77ad0de
Colby Ford <colbytylerford@gmail.com>
2018-03-03 16:36:01 -0500

    Corrected 'repeats' typo

--- RandomForestScript.R
+++ RandomForestScript.R
@@ -11,6 +11,9 @@ library(lime)
 #library(randomForest)
 library(ranger)
 
+memory.limit(size = NA)
+options(expressions = 5e5)
+
 ## Set up Parallelization
 library(doParallel)
 cl <- makeCluster(detectCores())
@@ -21,7 +24,7 @@ registerDoParallel(cl)
 CombinedData <- read_csv("datasets/DimensionalityReduction/DimensionalityReduction_CombinedData_OuterJoin.csv") #Outer Join Data
 colnames(CombinedData)[1] <- "TaxaID" #Fix <U+FEFF> Issues
 CombinedData[ CombinedData == "NULL" ] <- NA
-CombinedData$GuthrieZone <- as.factor(CombinedData$GuthrieZone)
+
 
 ## Create mtDNA Datasets
 mtDNA_cols <- paste0("mtDNA.pos", seq(1:16590))
@@ -32,6 +35,7 @@ mtDNAData <- CombinedData %>%
   separate(., mtDNA, mtDNA_cols, sep = seq(1:16590), remove = TRUE)
 # Remove NA Columns
 mtDNAData <- mtDNAData[!is.na(names(mtDNAData))]
+mtDNAData$GuthrieZone <- as.factor(mtDNAData$GuthrieZone)
 # Split for ML
 mtDNAData_trainIndex <- createDataPartition(mtDNAData$GuthrieZone, p = 0.50, list = FALSE)
 mtDNAData_train <- mtDNAData[ mtDNAData_trainIndex,]
@@ -46,6 +50,7 @@ YchrData <- CombinedData %>%
   separate(., Ychr_STR, Ychr_cols, sep = seq(1:12), remove = TRUE)
 # Remove NA Columns
 YchrData <- YchrData[!is.na(names(YchrData))]
+YchrData$GuthrieZone <- as.factor(YchrData$GuthrieZone)
 # Split for ML
 YchrData_trainIndex <- createDataPartition(YchrData$GuthrieZone, p = 0.50, list = FALSE)
 YchrData_train <- YchrData[ YchrData_trainIndex,]
@@ -59,6 +64,7 @@ GeneticData <- CombinedData %>%
   separate(., Ychr_STR, Ychr_cols, sep = seq(1:12), remove = TRUE)
 # Remove NA Columns
 GeneticData <- GeneticData[!is.na(names(GeneticData))]
+GeneticData$GuthrieZone <- as.factor(GeneticData$GuthrieZone)
 # Split for ML
 GeneticData_trainIndex <- createDataPartition(GeneticData$GuthrieZone, p = 0.50, list = FALSE)
 GeneticData_train <- GeneticData[ GeneticData_trainIndex,]
@@ -73,6 +79,7 @@ CulturalData <- CombinedData %>%
   separate(., Cultural_EthnogAtlas, Cultural_cols, sep = seq(1:92), remove = TRUE)
 # Remove NA Columns
 CulturalData <- CulturalData[!is.na(names(CulturalData))]
+CulturalData$GuthrieZone <- as.factor(CulturalData$GuthrieZone)
 # Split for ML
 CulturalData_trainIndex <- createDataPartition(CulturalData$GuthrieZone, p = 0.50, list = FALSE)
 CulturalData_train <- CulturalData[ CulturalData_trainIndex,]
@@ -87,6 +94,7 @@ CombinedData <- CombinedData %>%
   separate(., Cultural_EthnogAtlas, Cultural_cols, sep = seq(1:92), remove = TRUE)
 # Remove NA Columns
 CombinedData <- CombinedData[!is.na(names(CombinedData))]
+CombinedData$GuthrieZone <- as.factor(CombinedData$GuthrieZone)
 # Split for ML
 CombinedData_trainIndex <- createDataPartition(CombinedData$GuthrieZone, p = 0.50, list = FALSE)
 CombinedData_train <- CombinedData[ CombinedData_trainIndex,]
@@ -105,53 +113,53 @@ CombinedData_test <- CombinedData[-CombinedData_trainIndex,]
 ############
 
 ## Create Random Forest model and 10-fold CV
-Combined_model_cv <- caret::train(CombinedData_train,
-                                  CombinedData_train$GuthrieZone,
+Combined_model_cv <- caret::train(GuthrieZone ~ .,
+                                  data = CombinedData_train,
                                   method = 'ranger',
                                   metric = 'Accuracy',
                                   trControl = trainControl(method = "repeatedcv",
                                                            number = 10,
-                                                           times = 3,
+                                                           repeats = 3,
                                                            verboseIter = TRUE,
                                                            savePredictions = TRUE))
 
-mtDNA_model_cv <- caret::train(mtDNAData_train,
-                               mtDNAData_train$GuthrieZone,
+mtDNA_model_cv <- caret::train(GuthrieZone ~ .,
+                               data = mtDNAData_train,
                                method = 'ranger',
                                metric = 'Accuracy',
                                trControl = trainControl(method = "repeatedcv",
                                                         number = 10,
-                                                        times = 3,
+                                                        repeats = 3,
                                                         verboseIter = TRUE,
                                                         savePredictions = TRUE))
 
-Ychr_model_cv <- caret::train(YchrData_train,
-                              YchrData_train$GuthrieZone,
+Ychr_model_cv <- caret::train(GuthrieZone ~ .,
+                              data = YchrData_train,
                               method = 'ranger',
                               metric = 'Accuracy',
                               trControl = trainControl(method = "repeatedcv",
                                                        number = 10,
-                                                       times = 3,
+                                                       repeats = 3,
                                                        verboseIter = TRUE,
                                                        savePredictions = TRUE))
 
-Genetic_model_cv <- caret::train(GeneticData_train,
-                              GeneticData_train$GuthrieZone,
+Genetic_model_cv <- caret::train(GuthrieZone ~ .,
+                              data = GeneticData_train,
                               method = 'ranger',
                               metric = 'Accuracy',
                               trControl = trainControl(method = "repeatedcv",
                                                        number = 10,
-                                                       times = 3,
+                                                       repeats = 3,
                                                        verboseIter = TRUE,
                                                        savePredictions = TRUE))
 
-Cultural_model_cv <- caret::train(CulturalData_train,
-                                  CulturalData_train$GuthrieZone,
+Cultural_model_cv <- caret::train(GuthrieZone ~ .,
+                                  data = CulturalData_train,
                                   method = 'ranger',
                                   metric = 'Accuracy',
                                   trControl = trainControl(method = "repeatedcv",
                                                            number = 10,
-                                                           times = 3,
+                                                           repeats = 3,
                                                            verboseIter = TRUE,
                                                            savePredictions = TRUE))
 
